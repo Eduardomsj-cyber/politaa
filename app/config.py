fd994e75bfe2ca5e15dc6d7e7597edfb31b79283
Eduardomsj-cyber <138149064+Eduardomsj-cyber@users.noreply.github.com>
2025-12-04 04:09:36 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 import os
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key-change-me")
-    DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")
-    DB_PORT = int(os.environ.get("DB_PORT", "3306"))
-    DB_USER = os.environ.get("DB_USER", "root")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD", "1297")
-    DB_NAME = os.environ.get("DB_NAME", "foodpos")
+
+    # Datos de la DB de Railway
+    DB_HOST = os.environ.get("DB_HOST", "mysql.railway.internal")  # MYSQLHOST
+    DB_PORT = int(os.environ.get("DB_PORT", "3306"))               # MYSQLPORT
+    DB_USER = os.environ.get("DB_USER", "root")                    # MYSQLUSER
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "")                # MYSQLPASSWORD
+    DB_NAME = os.environ.get("DB_NAME", "railway")                 # MYSQLDATABASE
