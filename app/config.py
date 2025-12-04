df8becfa3e755c29eee87f92bbef9ffe472d9b90
Eduardomsj-cyber <138149064+Eduardomsj-cyber@users.noreply.github.com>
2025-12-04 11:00:59 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key-change-me")
 
     # Datos de la DB de Railway
-    DB_HOST = os.environ.get("DB_HOST", "mysql.railway.internal")  # MYSQLHOST
-    DB_PORT = int(os.environ.get("DB_PORT", "3306"))               # MYSQLPORT
+    DB_HOST = os.environ.get("DB_HOST", "tramway.proxy.rlwy.net")  # MYSQLHOST
+    DB_PORT = int(os.environ.get("DB_PORT", "27269"))               # MYSQLPORT
     DB_USER = os.environ.get("DB_USER", "root")                    # MYSQLUSER
-    DB_PASSWORD = os.environ.get("DB_PASSWORD", "sRGTfmzxPmVNXckNTrqXkjTMYMmwFFMY")                # MYSQLPASSWORD
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "fGOloFvTzuXyEUbIcJxFjaFbyPYtOXLg")                # MYSQLPASSWORD
     DB_NAME = os.environ.get("DB_NAME", "railway")                 # MYSQLDATABASE
