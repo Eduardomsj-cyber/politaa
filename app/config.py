c0dd757c45fc15334ac1a1def01861f90d7733f0
Eduardomsj-cyber <138149064+Eduardomsj-cyber@users.noreply.github.com>
2025-12-04 04:10:20 -0600

    Change default DB_PASSWORD in config.py
    Updated default DB_PASSWORD to a specific value.

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ class Config:
     DB_HOST = os.environ.get("DB_HOST", "mysql.railway.internal")  # MYSQLHOST
     DB_PORT = int(os.environ.get("DB_PORT", "3306"))               # MYSQLPORT
     DB_USER = os.environ.get("DB_USER", "root")                    # MYSQLUSER
-    DB_PASSWORD = os.environ.get("DB_PASSWORD", "")                # MYSQLPASSWORD
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "sRGTfmzxPmVNXckNTrqXkjTMYMmwFFMY")                # MYSQLPASSWORD
     DB_NAME = os.environ.get("DB_NAME", "railway")                 # MYSQLDATABASE
